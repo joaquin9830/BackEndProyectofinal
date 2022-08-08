761b0e5dccb6bed0218fa554290fd883c3930ee1
joaquin9830 <joaquin9811@gmail.com>
2022-08-08 15:09:33 -0300

    Delete de experiencia añadido y funcional

--- VJ/src/main/java/com/portfoliovi/mgb/Controller/CExperiencia.java
+++ VJ/src/main/java/com/portfoliovi/mgb/Controller/CExperiencia.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -80,6 +81,7 @@ public class CExperiencia {
         return new ResponseEntity(new Mensaje("Experiencia actualizada"), HttpStatus.OK);
     }
     
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id){
         //VALIDACIÓN DE ID
         if(!sExperiencia.existsById(id))
